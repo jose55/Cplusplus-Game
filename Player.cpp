b2dd43895ffde77f1824303930708462b03c2880
André Andersson <andre.andersson@falun.ntig.se>
2013-03-05 01:01:34 +0100

    Collision Detection Update 2
    It's partially working now. Better than before atleast. It still enters
    walls but only one square from the cave path.

--- Player.cpp
+++ Player.cpp
@@ -19,12 +19,14 @@ int Player(int x, int y, int Room[50][50], int n)
 
 	while(playerhealth > 0 && playerlives > 0 ) // Medans spelaren är vid liv och har minst 1 liv kvar
 	{
-		Room[playerX][playerY] = 0;
+		
 		playerdir = _getch();
+		Room[playerX][playerY] = 0;
 
 		// Om användaren trycker på W eller w 
 		if((playerdir == 119 || playerdir == 87) && Room[playerX - 1][playerY] == 0)
 		{
+			
 			playerX --;
 			Room[playerX][playerY] = 3;
 		}
@@ -32,20 +34,23 @@ int Player(int x, int y, int Room[50][50], int n)
 		// Om användaren trycker på S eller s
 		else if((playerdir == 115 || playerdir == 83) && Room[playerX + 1][playerY] == 0)
 		{
+			
 			playerX ++;
 			Room[playerX][playerY] = 3;
 		}
 		// Om användaren trycker på A eller a
 		else if((playerdir == 97 || playerdir == 65) && Room[playerX][playerY - 1] == 0)
 		{
+			
 			playerY --;
 			Room[playerX][playerY] = 3;
 		}
 		// Om användaren trycker på D eller d
 		else if((playerdir == 100 || playerdir == 68) && Room[playerX][playerY + 1] == 0)
 		{
+			
 			playerY ++;
-			Room[playerX][playerY + 1] = 3;
+			Room[playerX][playerY] = 3;
 		}
 		else{}
 		cout << playerX << "	" << playerY;
